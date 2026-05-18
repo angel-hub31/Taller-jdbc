56004e62ec60ad680f0ef4e404511f735d88d29f
angel-hub31 <moralesmales1@hotmail.com>
2026-05-17 20:37:04 -0500

    se crea la clase Vehiculo con sus atributos privados

---
+++ src/main/java/com/krakedev/entidades/Vehiculo.java
@@ -0,0 +1,12 @@
+package com.krakedev.entidades;
+
+public class Vehiculo {
+	private String placa;
+	private String marca;
+	private String modelo;
+	private int anio;
+	private double precio;
+	private String color;
+	private boolean disponible;
+	
+}
