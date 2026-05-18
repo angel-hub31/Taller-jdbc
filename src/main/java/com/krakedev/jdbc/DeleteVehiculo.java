0424b7397622b9751efdf8dbb08f239ebd1dc9bb
angel-hub31 <moralesmales1@hotmail.com>
2026-05-17 23:35:42 -0500

    seteamos para borrar mediante la llave primaria en este caso la placa

--- src/main/java/com/krakedev/jdbc/DeleteVehiculo.java
+++ src/main/java/com/krakedev/jdbc/DeleteVehiculo.java
@@ -1,5 +1,8 @@
 package com.krakedev.jdbc;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,7 +11,26 @@ public class DeleteVehiculo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Connection con= null;
+		PreparedStatement ps = null;
+		
+		try {
+			con=Conexion.getConnection();
+			String sql="DELETE FROM Vehiculos WHERE placa=?";
+			
+			ps=con.prepareStatement(sql);
+			
+			ps=con.prepareStatement(sql);
+			ps.setString(1, "PSG-123");
+			
+			int filas=ps.executeUpdate();
+			
+			log.info("Filas eliminadas: " + filas);
+			
+			
+		}catch(Exception e) {
+			
+		}
 	}
 
 }
