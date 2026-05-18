4c52842656784bcafc592aec6073fced857cd31d
angel-hub31 <moralesmales1@hotmail.com>
2026-05-18 00:44:05 -0500

    Se agrega un nuevo atributo en la clase Vehiculo y se actualizatoda la clase Vehiculo

--- src/main/java/com/krakedev/entidades/Vehiculo.java
+++ src/main/java/com/krakedev/entidades/Vehiculo.java
@@ -7,6 +7,7 @@ public class Vehiculo {
 	private int anio;
 	private double precio;
 	private String color;
+	private int kilometraje;
 	private boolean disponible;
 
 	// constructor vacio
@@ -15,7 +16,7 @@ public class Vehiculo {
 	}
 
 	// cosntructor que recibe parametros
-	public Vehiculo(String placa, String marca, String modelo, int anio, double precio, String color,
+	public Vehiculo(String placa, String marca, String modelo, int anio, double precio, String color,int kilometraje,
 			boolean disponible) {
 
 		this.placa = placa;
@@ -24,6 +25,73 @@ public class Vehiculo {
 		this.anio = anio;
 		this.precio = precio;
 		this.color = color;
+		this.kilometraje=kilometraje;
+		this.disponible = disponible;
+	}
+	
+	//getters y setters
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public int getAnio() {
+		return anio;
+	}
+
+	public void setAnio(int anio) {
+		this.anio = anio;
+	}
+
+	public double getPrecio() {
+		return precio;
+	}
+
+	public void setPrecio(double precio) {
+		this.precio = precio;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public int getKilometraje() {
+		return kilometraje;
+	}
+
+	public void setKilometraje(int kilometraje) {
+		this.kilometraje = kilometraje;
+	}
+
+	public boolean isDisponible() {
+		return disponible;
+	}
+
+	public void setDisponible(boolean disponible) {
 		this.disponible = disponible;
 	}
 
@@ -31,7 +99,7 @@ public class Vehiculo {
 	@Override
 	public String toString() {
 		return "Vehiculo [placa=" + placa + ", marca=" + marca + ", modelo=" + modelo + ", anio=" + anio + ", precio="
-				+ precio + ", color=" + color + ", disponible=" + disponible + "]";
+				+ precio + ", color=" + color + ", kilometraje="+kilometraje+", disponible=" + disponible + "]";
 	}
 
 }
