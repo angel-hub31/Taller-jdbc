6bdb387ab014a76ced4b5b3820a3aa0928736738
angel-hub31 <moralesmales1@hotmail.com>
2026-05-17 20:38:55 -0500

    se crea un constructor vacio y un constructor que recibe parametros

--- src/main/java/com/krakedev/entidades/Vehiculo.java
+++ src/main/java/com/krakedev/entidades/Vehiculo.java
@@ -8,5 +8,23 @@ public class Vehiculo {
 	private double precio;
 	private String color;
 	private boolean disponible;
-	
+
+	// constructor vacio
+	public Vehiculo() {
+
+	}
+
+	// cosntructor que recibe parametros
+	public Vehiculo(String placa, String marca, String modelo, int anio, double precio, String color,
+			boolean disponible) {
+
+		this.placa = placa;
+		this.marca = marca;
+		this.modelo = modelo;
+		this.anio = anio;
+		this.precio = precio;
+		this.color = color;
+		this.disponible = disponible;
+	}
+
 }
