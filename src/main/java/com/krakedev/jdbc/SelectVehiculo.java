899bc1acfab0673e60a7127f25a9893183eb8174
angel-hub31 <moralesmales1@hotmail.com>
2026-05-17 22:59:23 -0500

    realizamos correciones de conexion

--- src/main/java/com/krakedev/jdbc/SelectVehiculo.java
+++ src/main/java/com/krakedev/jdbc/SelectVehiculo.java
@@ -10,20 +10,19 @@ import org.slf4j.LoggerFactory;
 
 public class SelectVehiculo {
 	private static final Logger log = LoggerFactory.getLogger(SelectVehiculo.class);
-	private static final String URL = "jdbc:postgresql://localhost:5432/tallerjdbc";
-	private static final String USER = "postgres";
-	private static final String PASSWORD = "1004034391";
 
 	public static void main(String[] args) {
 		// realizamos conexion co la base de datos, lo inicializamos con null
 		Connection con = null;
 		// representa una sentencia sql preparada
-		PreparedStatement ps;
+		PreparedStatement ps = null;
 		// ResultSet guarda las filas devueltas o importadas por el select
 		ResultSet rs = null;
 
 		try {
-
+			// llamamos a nuestro metodo Connection de nuestra clase Conexion
+			// y llamamos al metodo statico
+			con = Conexion.getConnection();
 			// sentencias SQL
 			String sql = """
 					SELECT * FROM vehiculos
@@ -48,9 +47,9 @@ public class SelectVehiculo {
 			}
 
 		} catch (Exception e) {
-			log.error("error al traer datos",e.getMessage());
+			log.error("error al traer datos", e.getMessage());
 
-		}finally {
+		} finally {
 			try {
 				con.close();
 			} catch (SQLException e) {
