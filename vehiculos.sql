5e2c97ca5771833989555bf56522ea67fe341cf2
angel-hub31 <moralesmales1@hotmail.com>
2026-05-17 20:21:27 -0500

    se crea la baase de datos llamado vehiculos en Postgres

---
+++ vehiculos.sql
@@ -0,0 +1,11 @@
+drop table vehiculos;
+create table vehiculos(
+placa varchar (10) primary key,
+marca varchar (50) not null,
+modelo varchar (50) not null,
+anio int not null,
+precio double precision not null,
+color varchar (30),
+disponible boolean not null
+
+);
