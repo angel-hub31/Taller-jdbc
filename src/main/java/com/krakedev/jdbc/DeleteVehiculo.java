0669e4e4bafcb3d32ea832d22c7ea776e2791f80
angel-hub31 <moralesmales1@hotmail.com>
2026-05-17 23:31:16 -0500

    se crea la clase DeleteVehiculo

---
+++ src/main/java/com/krakedev/jdbc/DeleteVehiculo.java
@@ -0,0 +1,14 @@
+package com.krakedev.jdbc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DeleteVehiculo {
+	private static final Logger log = LoggerFactory.getLogger(DeleteVehiculo.class);
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
